7b66aa325d0a5d499f8b6118dae6441510dc1cd8
Jan Nyman <jan.nyman@controlthings.fi>
2020-02-03 16:12:26 +0200

    Add copyright banner

--- src/port_relay_client.h
+++ src/port_relay_client.h
@@ -1,3 +1,14 @@
+/**
+ * Copyright (C) 2020, ControlThings Oy Ab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @license Apache-2.0
+ */
 #pragma once
 
 void port_relay_client_open(wish_core_t *core, wish_relay_client_t* relay, wish_ip_addr_t *ip);
